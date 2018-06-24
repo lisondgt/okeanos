10d7b04d5a5c501f2578ffdb1ed1ba0194bf6842
Scwaall <fischer.pascal@wanadoo.fr>
2018-06-24 14:05:53 +0200

    22-06-2018

--- src/Okeanos/CoreBundle/Controller/DefaultController.php
+++ src/Okeanos/CoreBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace Okeanos\CoreBundle\Controller;
 
+use Okeanos\CoreBundle\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -14,19 +17,74 @@ class DefaultController extends Controller
 
     public function newsAction()
     {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OkeanosCoreBundle:News');
+        
+        $news = $repository->findAll();
+
         $content = $this
             ->get('templating')
-            ->render('OkeanosCoreBundle:Default:news.html.twig');
+            ->render('OkeanosCoreBundle:Default:news.html.twig',
+                array('news' => $news));
 
         return new Response($content);
     }
 
     public function newsViewAction($id)
     {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OkeanosCoreBundle:News');
+
+        $news = $repository->find($id);
+
+        if(null === $news)
+        {
+            throw new NotFoundHttpException("Cette news n'existe pas");
+        }
+
         $content = $this
             ->get('templating')
-            ->render('OkeanosCoreBundle:Default:newsView.html.twig');
+            ->render('OkeanosCoreBundle:Default:newsView.html.twig',
+                array('news' => $news));
 
+        return new Response($content);
+    }
+
+    public function newsAddAction(Request $request){
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Création de l'entité Advert
+        $news = new News();
+        $news->setTitle('Recherche intégrateur AngularJS.');
+        $news->setDescript("Nous recherchons un intégrateur sur Dijon. Blabla...");
+        $news->setImg("#");
+        $news->setDate(new \Datetime());
+
+        $em->persist($news);
+
+        // On déclenche l'enregistrement
+        $em->flush();
+
+        // La gestion d'un formulaire est particulière mais l'idée est la suivante :
+
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if($request->isMethod('POST')){
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+            $request->getSession()->getFlashBag()->add('notice', 'News bien enregistrée');
+
+            // Puis on redirige vers la page de visualisation de cette annonce
+            return $this->redirectToRoute('okeanos_core_newsView', array('id' => $news->getId()));
+        }
+
+        $content = $this
+            ->get('templating')
+            ->render('OkeanosCoreBundle:Default:newsAdd.html.twig');
+        
         return new Response($content);
     }
 
