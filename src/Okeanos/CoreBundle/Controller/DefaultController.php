b223edaf508bf33e39b0f9999053ef4cbd0586f0
Scwaall <fischer.pascal@wanadoo.fr>
2018-06-24 16:32:13 +0200

    Controleur : Ajout du "actionsAddAction" pour ajouter une action. Route : Ajout du "actionsAdd" pour accéder à la page d'ajout d'action. Vues : Modification des vues pour afficher toutes les actions dans "actions" et seulement l'action consultée dans "actionsView".

--- src/Okeanos/CoreBundle/Controller/DefaultController.php
+++ src/Okeanos/CoreBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Okeanos\CoreBundle\Controller;
 
 use Okeanos\CoreBundle\Entity\News;
+use Okeanos\CoreBundle\Entity\Actions;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -90,20 +91,75 @@ class DefaultController extends Controller
 
     public function actionsAction()
     {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OkeanosCoreBundle:Actions');
+        
+        $actions = $repository->findAll();
+
         $content = $this
             ->get('templating')
-            ->render('OkeanosCoreBundle:Default:actions.html.twig');
+            ->render('OkeanosCoreBundle:Default:actions.html.twig',
+                array('actions' => $actions));
 
         return new Response($content);
     }
 
     public function actionsViewAction($id)
     {
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OkeanosCoreBundle:Actions');
+
+        $action = $repository->find($id);
+
+        if(null === $action)
+        {
+            throw new NotFoundHttpException("Cette action n'existe pas");
+        }
+
         $content = $this
             ->get('templating')
             ->render('OkeanosCoreBundle:Default:actionsView.html.twig',
-                array('action' => $id));
+                array('action' => $action));
 
+        return new Response($content);
+    }
+
+    public function actionsAddAction(Request $request){
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Création de l'entité Advert
+        $action = new Actions();
+        $action->setName('Action 1');
+        $action->setDescript("Description de l'action 1");
+        $action->setImg("#");
+        $action->setGoal(10000);
+        $action->setStatus(false);
+
+        $em->persist($action);
+
+        // On déclenche l'enregistrement
+        $em->flush();
+
+        // La gestion d'un formulaire est particulière mais l'idée est la suivante :
+
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if($request->isMethod('POST')){
+            // Ici, on s'occupera de la création et de la gestion du formulaire
+            $request->getSession()->getFlashBag()->add('notice', 'Action bien enregistrée');
+
+            // Puis on redirige vers la page de visualisation de cette annonce
+            return $this->redirectToRoute('okeanos_core_actionsView', array('id' => $action->getId()));
+        }
+
+        $content = $this
+            ->get('templating')
+            ->render('OkeanosCoreBundle:Default:actionsAdd.html.twig');
+        
         return new Response($content);
     }
 
